85d82d4ff2f3e522c99ca7ebbcf3cdc21b41bea6
lezernack <luks.ezck@gmail.com>
2024-02-08 12:26:21 -0700

    finished the code

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const cards = document.querySelectorAll(".memory-card");
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
   this.classList.toggle("flip");
 
   if (!hasFlippedCard) {
     // first click
     hasFlippedCard = true;
     firstCard = this;
-  } else {
-    // second click
-    hasFlippedCard = false;
-    secondCard = this;
 
-    checkForMatch();
+    return;
   }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
 }
 
 function checkForMatch() {
@@ -28,13 +33,31 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
 function unFlipCards() {
+  lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
+
+    resetBoard();
   }, 1500);
 }
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
